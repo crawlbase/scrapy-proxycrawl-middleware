64d5f1d80d24c0e1a09a63aad38566c82ecc5f31
proxycrawl <info@proxycrawl.com>
2019-01-23 21:09:16 +0800

    Style change

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 import sys
 from distutils.core import setup
 
-setup(name='scrapy-proxycrawl-middleware',
-        version='1.0',
-        description='Scrapy ProxyCrawl Proxy Middleware: ProxyCrawl interfacing middleware for Scrapy',
-        author='ProxyCrawl',
-        author_email='info@proxycrawl.com',
-        url='https://github.com/proxycrawl/scrapy-proxycrawl-middleware',
-        packages=['scrapy_proxycrawl'],
-        )
+setup(name = 'scrapy-proxycrawl-middleware',
+    version = '1.0',
+    description = 'Scrapy ProxyCrawl Proxy Middleware: ProxyCrawl interfacing middleware for Scrapy',
+    author = 'ProxyCrawl',
+    author_email = 'info@proxycrawl.com',
+    url = 'https://github.com/proxycrawl/scrapy-proxycrawl-middleware',
+    packages = ['scrapy_proxycrawl'],
+)
