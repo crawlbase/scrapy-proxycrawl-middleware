42199e68938d857afac5c21dc9ee26f66c2bbcb5
proxycrawl <info@proxycrawl.com>
2019-02-05 00:15:54 +0800

    Improve metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
+
+"""ProxyCrawl Scrapy middleware."""
+
 import sys
+import os
 from distutils.core import setup
 
+readme = open('README.md').read()
+
 setup(name = 'scrapy-proxycrawl-middleware',
-    version = '1.0',
+    license = 'Apache-2.0',
+    version = '1.0.0',
     description = 'Scrapy ProxyCrawl Proxy Middleware: ProxyCrawl interfacing middleware for Scrapy',
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
     author = 'ProxyCrawl',
     author_email = 'info@proxycrawl.com',
     url = 'https://github.com/proxycrawl/scrapy-proxycrawl-middleware',
+    keywords = 'scrapy middleware scraping scraper crawler crawling proxycrawl api',
     packages = ['scrapy_proxycrawl'],
+    include_package_data = True,
+    classifiers = (
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+    )
 )
